f53eb3f452a77f5d21ecef210c6ed38ab230b5f0
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-01-02 17:18:06 +0100

    generate configurations for crl

--- src/gcrl_landscapes/main.py
+++ src/gcrl_landscapes/main.py
@@ -2,8 +2,8 @@ from training import full_phased_run
 from evaluate import evaluate_wrapper
 import ogbench
 from ogbench.impls.utils.datasets import GCDataset, Dataset
-from ogbench.impls.agents.crl import CRLAgent, get_config
-from ml_collections import FrozenConfigDict
+from ogbench.impls.agents.crl import CRLAgent
+from configurations import generate_configurations_crl
 
 if __name__ == "__main__":
     # [TODO: make this configurable by argument parser or loop over multiple setups]
@@ -12,21 +12,24 @@ if __name__ == "__main__":
     env, train_dataset, val_dataset = ogbench.make_env_and_datasets(
         "humanoidmaze-large-navigate-v0"
     )  # type: ignore
-    train_steps = 1000
-    eval_at_steps = [500]
-    config = get_config()
-    save_at_steps = [500]
+    configurations = generate_configurations_crl(128)
 
     results_per_phase = full_phased_run(
-        phase_steps=[10, 20, 30, 40, 100],
-        configs=[FrozenConfigDict(config)],
+        phase_steps=[10, 20],
+        configs=configurations,
         agent_class=CRLAgent,  # type: ignore # types of ogbench are not properly defined
         env=env,
-        train_dataset=GCDataset(Dataset.create(**train_dataset), config),
-        val_dataset=GCDataset(Dataset.create(**val_dataset), config),
-        eval_at_steps=[10, 20, 30, 40, 50, 100],
+        train_datasets=[
+            GCDataset(Dataset.create(**train_dataset), config)
+            for config in configurations
+        ],
+        val_datasets=[
+            GCDataset(Dataset.create(**val_dataset), config)
+            for config in configurations
+        ],
+        eval_at_steps=[10, 20],
         evaluate=evaluate_wrapper,
-        save_at_steps=[10, 20, 30, 40, 100],
+        save_at_steps=[10, 20],
         eval_episodes=2,
     )
     print(results_per_phase)
