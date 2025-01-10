b8aba70949cbb138d6c8ec69f28d722d9340b8f9
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-01-10 14:13:22 +0100

    train.sh with updated values for using argparse

--- train.sh
+++ train.sh
@@ -6,4 +6,4 @@ export MUJOCO_GL=egl
 # cache autotuning results to not having to recompile them on every run
 export JAX_COMPILATION_CACHE_DIR="/tmp/jax_cache"
 
-python src/gcrl_landscapes/main.py
+python src/gcrl_landscapes/main.py --agent CRL --dataset antmaze-medium-navigate-v0 --n_configurations 32 --phase_steps 50000 100000 200000 --eval_steps 10000 50000 100000 200000 500000 --eval_episodes 10
