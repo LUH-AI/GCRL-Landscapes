be6af7f5e3bfe5715dd3fbb42d76d32795b59a92
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-01-10 14:13:44 +0100

    up max time of slurm job

--- run_slurm.sh
+++ run_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=32G
 #SBATCH -o slurm_outputs/slurm-%j.out
 #SBATCH --partition=ai
-#SBATCH --time=16:00:00
+#SBATCH --time=48:00:00
 #SBATCH --gres=gpu:1
 
 #SBATCH --mail-user=m.toepperwien@stud.uni-hannover.de
