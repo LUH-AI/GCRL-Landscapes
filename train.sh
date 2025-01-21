75351cef87e08db1de14b17a1b780491ab15fbd9
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-01-21 11:31:22 +0100

    run with sac

--- train.sh
+++ train.sh
@@ -6,4 +6,4 @@ export MUJOCO_GL=egl
 # cache autotuning results to not having to recompile them on every run
 export JAX_COMPILATION_CACHE_DIR="/tmp/jax_cache"
 
-python src/gcrl_landscapes/main.py --agent CMD --dataset antmaze-medium-navigate-v0 --n_configurations 32 --phase_steps 50000 100000 150000 --eval_steps 10000 50000 100000 150000 200000 --eval_episodes 10
+python src/gcrl_landscapes/main.py --agent SAC --dataset antmaze-medium-navigate-v0 --n_configurations 32 --phase_steps 50000 100000 150000 --eval_steps 10000 50000 100000 150000 200000 --eval_episodes 10
