0ca7be737f1a5a6828f93f5b2617b3f4db63364f
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-01-21 09:26:07 +0100

    support sac runs

--- src/gcrl_landscapes/configurations.py
+++ src/gcrl_landscapes/configurations.py
@@ -3,6 +3,7 @@ from scipy.stats.qmc import Sobol
 from math import log
 import ogbench.impls.agents.crl
 import ogbench.impls.agents.cmd
+import ogbench.impls.agents.sac
 
 # [TODO: is it okay that this is different from the paper? With higher learning rates we get overflows]
 LEARNING_RATE_LOWER = 1e-6
@@ -13,17 +14,39 @@ DISCOUNT_FACTOR_UPPER = 0.99
 
 
 def generate_configurations(n: int, agent: str) -> list[FrozenConfigDict]:
-    agent_config_generators = {"CRL": generate_configurations_crl, "CMD": generate_configurations_cmd }
-    return agent_config_generators[agent](n)
-
-def generate_configurations_cmd(n: int) -> list[FrozenConfigDict]:
-    # [TODO: this is only copy and paste, take a look if it is actually right and rewrite it]
     ld_n = log(n, 2)
     if not ld_n.is_integer():
         raise ValueError("Only supports powers of 2")
 
+    agent_config_generators = {
+        "CRL": _generate_configurations_crl,
+        "CMD": _generate_configurations_cmd,
+        "SAC": _generate_configurations_sac,
+    }
+    return agent_config_generators[agent](n)
+
+
+def _generate_configurations_sac(n: int) -> list[FrozenConfigDict]:
     sobol_generator = Sobol(2)
-    sobol_sample = sobol_generator.random_base2(round(ld_n))
+    sobol_sample = sobol_generator.random_base2(round(log(n, 2)))
+    learning_rates = (
+        sobol_sample[:, 0] * (LEARNING_RATE_UPPER - LEARNING_RATE_LOWER)
+        + LEARNING_RATE_LOWER
+    )
+    discount_factors = (
+        sobol_sample[:, 1] * (DISCOUNT_FACTOR_UPPER - DISCOUNT_FACTOR_LOWER)
+        + DISCOUNT_FACTOR_LOWER
+    )
+    base_config = ogbench.impls.agents.sac.get_config().to_dict()
+    return [
+        FrozenConfigDict(initial_dictionary=base_config | {"lr": lr, "discount": df})
+        for lr, df in zip(learning_rates, discount_factors)
+    ]
+
+
+def _generate_configurations_cmd(n: int) -> list[FrozenConfigDict]:
+    sobol_generator = Sobol(2)
+    sobol_sample = sobol_generator.random_base2(round(log(n, 2)))
     learning_rates = (
         sobol_sample[:, 0] * (LEARNING_RATE_UPPER - LEARNING_RATE_LOWER)
         + LEARNING_RATE_LOWER
@@ -38,13 +61,10 @@ def generate_configurations_cmd(n: int) -> list[FrozenConfigDict]:
         for lr, df in zip(learning_rates, discount_factors)
     ]
 
-def generate_configurations_crl(n: int) -> list[FrozenConfigDict]:
-    ld_n = log(n, 2)
-    if not ld_n.is_integer():
-        raise ValueError("Only supports powers of 2")
 
+def _generate_configurations_crl(n: int) -> list[FrozenConfigDict]:
     sobol_generator = Sobol(2)
-    sobol_sample = sobol_generator.random_base2(round(ld_n))
+    sobol_sample = sobol_generator.random_base2(round(log(n, 2)))
     learning_rates = (
         sobol_sample[:, 0] * (LEARNING_RATE_UPPER - LEARNING_RATE_LOWER)
         + LEARNING_RATE_LOWER
