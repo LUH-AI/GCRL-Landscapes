43ccbe6f7c31a140bc7a46497e23e3b7a248d331
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-01-24 10:03:22 +0100

    add support for gcbc, qrl, hiql

--- src/gcrl_landscapes/configurations.py
+++ src/gcrl_landscapes/configurations.py
@@ -3,6 +3,9 @@ from scipy.stats.qmc import Sobol
 from math import log
 import ogbench.impls.agents.crl
 import ogbench.impls.agents.cmd
+import ogbench.impls.agents.gcbc
+import ogbench.impls.agents.qrl
+import ogbench.impls.agents.hiql
 
 # [TODO: is it okay that this is different from the paper? With higher learning rates we get overflows]
 LEARNING_RATE_LOWER = 1e-6
@@ -18,43 +21,43 @@ def generate_configurations(n: int, agent: str) -> list[FrozenConfigDict]:
         raise ValueError("Only supports powers of 2")
 
     agent_config_generators = {
-        "CRL": _generate_configurations_crl,
-        "CMD": _generate_configurations_cmd,
+        "CRL": lambda n: _generate_configurations(
+            ogbench.impls.agents.crl.get_config().to_dict(), n
+        ),
+        "CMD": lambda n: _generate_configurations(
+            ogbench.impls.agents.cmd.get_config().to_dict(), n
+        ),
+        "GCBC": lambda n: _generate_configurations(
+            ogbench.impls.agents.gcbc.get_config().to_dict(), n
+        ),
+        "QRL": lambda n: _generate_configurations(
+            ogbench.impls.agents.qrl.get_config().to_dict(), n
+        ),
+        "HIQL": lambda n: _generate_configurations(
+            ogbench.impls.agents.hiql.get_config().to_dict(), n
+        ),
     }
     return agent_config_generators[agent](n)
 
 
-def _generate_configurations_cmd(n: int) -> list[FrozenConfigDict]:
-    sobol_generator = Sobol(2)
-    sobol_sample = sobol_generator.random_base2(round(log(n, 2)))
-    learning_rates = (
-        sobol_sample[:, 0] * (LEARNING_RATE_UPPER - LEARNING_RATE_LOWER)
-        + LEARNING_RATE_LOWER
-    )
-    discount_factors = (
-        sobol_sample[:, 1] * (DISCOUNT_FACTOR_UPPER - DISCOUNT_FACTOR_LOWER)
-        + DISCOUNT_FACTOR_LOWER
-    )
-    base_config = ogbench.impls.agents.cmd.get_config().to_dict()
+def _generate_configurations(base_config: dict, n: int):
     return [
         FrozenConfigDict(initial_dictionary=base_config | {"lr": lr, "discount": df})
-        for lr, df in zip(learning_rates, discount_factors)
+        for lr, df in zip(_generate_learning_rates(n), _generate_discount_factors(n))
     ]
 
 
-def _generate_configurations_crl(n: int) -> list[FrozenConfigDict]:
-    sobol_generator = Sobol(2)
-    sobol_sample = sobol_generator.random_base2(round(log(n, 2)))
-    learning_rates = (
-        sobol_sample[:, 0] * (LEARNING_RATE_UPPER - LEARNING_RATE_LOWER)
+def _generate_learning_rates(n: int) -> list[float]:
+    return list(
+        Sobol(1).random_base2(round(log(n, 2)))[0]
+        * (LEARNING_RATE_UPPER - LEARNING_RATE_LOWER)
         + LEARNING_RATE_LOWER
     )
-    discount_factors = (
-        sobol_sample[:, 1] * (DISCOUNT_FACTOR_UPPER - DISCOUNT_FACTOR_LOWER)
+
+
+def _generate_discount_factors(n: int) -> list[float]:
+    return list(
+        Sobol(1).random_base2(round(log(n, 2)))[0]
+        * (DISCOUNT_FACTOR_UPPER - DISCOUNT_FACTOR_LOWER)
         + DISCOUNT_FACTOR_LOWER
     )
-    base_config = ogbench.impls.agents.crl.get_config().to_dict()
-    return [
-        FrozenConfigDict(initial_dictionary=base_config | {"lr": lr, "discount": df})
-        for lr, df in zip(learning_rates, discount_factors)
-    ]
