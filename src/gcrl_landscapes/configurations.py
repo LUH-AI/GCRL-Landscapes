7b1bfc74cdf61f22b00147afd5f1eb4bc7a350f1
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-01-24 09:54:05 +0100

    remove support for sac, as it is not supported by ogbench

--- src/gcrl_landscapes/configurations.py
+++ src/gcrl_landscapes/configurations.py
@@ -3,7 +3,6 @@ from scipy.stats.qmc import Sobol
 from math import log
 import ogbench.impls.agents.crl
 import ogbench.impls.agents.cmd
-import ogbench.impls.agents.sac
 
 # [TODO: is it okay that this is different from the paper? With higher learning rates we get overflows]
 LEARNING_RATE_LOWER = 1e-6
@@ -21,27 +20,8 @@ def generate_configurations(n: int, agent: str) -> list[FrozenConfigDict]:
     agent_config_generators = {
         "CRL": _generate_configurations_crl,
         "CMD": _generate_configurations_cmd,
-        "SAC": _generate_configurations_sac,
     }
     return agent_config_generators[agent](n)
-
-
-def _generate_configurations_sac(n: int) -> list[FrozenConfigDict]:
-    sobol_generator = Sobol(2)
-    sobol_sample = sobol_generator.random_base2(round(log(n, 2)))
-    learning_rates = (
-        sobol_sample[:, 0] * (LEARNING_RATE_UPPER - LEARNING_RATE_LOWER)
-        + LEARNING_RATE_LOWER
-    )
-    discount_factors = (
-        sobol_sample[:, 1] * (DISCOUNT_FACTOR_UPPER - DISCOUNT_FACTOR_LOWER)
-        + DISCOUNT_FACTOR_LOWER
-    )
-    base_config = ogbench.impls.agents.sac.get_config().to_dict()
-    return [
-        FrozenConfigDict(initial_dictionary=base_config | {"lr": lr, "discount": df})
-        for lr, df in zip(learning_rates, discount_factors)
-    ]
 
 
 def _generate_configurations_cmd(n: int) -> list[FrozenConfigDict]:
