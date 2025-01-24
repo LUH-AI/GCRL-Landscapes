a51aeb553547f9d479a5179f606b9ee341532745
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-01-24 09:54:05 +0100

    remove support for sac, as it is not supported by ogbench

--- src/gcrl_landscapes/main.py
+++ src/gcrl_landscapes/main.py
@@ -2,14 +2,14 @@ from training import full_phased_run
 from evaluate import evaluate_wrapper
 import ogbench
 from ogbench.impls.utils.datasets import GCDataset, Dataset
-from ogbench.impls.agents import CRLAgent, CMDAgent, SACAgent
+from ogbench.impls.agents import CRLAgent, CMDAgent
 from configurations import generate_configurations
 import argparse
 from datetime import datetime
 from pathlib import Path
 import json
 
-AGENT_CLASSES = {"CRL": CRLAgent, "CMD": CMDAgent, "SAC": SACAgent}
+AGENT_CLASSES = {"CRL": CRLAgent, "CMD": CMDAgent}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
