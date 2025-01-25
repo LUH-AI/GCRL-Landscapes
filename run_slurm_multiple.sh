b7bb411261a2097e16b8bbeefcaa7631b9cf2151
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-01-25 12:57:36 +0100

    run-script for multiple slurm experiments

---
+++ run_slurm_multiple.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for dataset in "antmaze-medium-navigate-v0" "antmaze-medium-explore-v0" "antmaze-medium-stitch-v0"; do
+  for agent in "CRL" "GCBC" "QRL" "HIQL"; do
+    ./run_slurm.sh "$agent" "$dataset"
+  done
+done
