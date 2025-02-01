b1bd487e4c97bc7135336f2882a777acb9a84a26
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-01 16:32:41 +0100

    experiments on large dataset

--- run_slurm_multiple.sh
+++ run_slurm_multiple.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for dataset in "antmaze-medium-navigate-v0" "antmaze-medium-explore-v0" "antmaze-medium-stitch-v0"; do
+for dataset in "antmaze-large-navigate-v0" "antmaze-large-explore-v0" "antmaze-large-stitch-v0"; do
   for agent in "CRL" "GCBC" "QRL" "HIQL"; do
     sbatch run_slurm.sh "$agent" "$dataset"
   done
