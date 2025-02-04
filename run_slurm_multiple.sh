d23ad75cd17481fae10b19b4e013cdf2b758f2a3
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-04 11:17:56 +0100

    fix two small bugs

--- run_slurm_multiple.sh
+++ run_slurm_multiple.sh
@@ -9,6 +9,6 @@
 # new experiments look at actor_p_trajgoal and gcbc or crl
 for dataset in "antmaze-large-navigate-v0" "antmaze-large-explore-v0" "antmaze-large-stitch-v0"; do
   for agent in "CRL" "GCBC"; do
-    sbatch run_slurm.sh "$agent" "$dataset" "discount agent_p_trajgoal"
+    sbatch run_slurm.sh "$agent" "$dataset" "discount actor_p_trajgoal"
   done
 done
