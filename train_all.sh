eb99ef8d5d3a1d7eb60fc968d9cdf75d8f7252a3
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-10 17:37:34 +0100

    add all partitions

--- train_all.sh
+++ train_all.sh
@@ -3,6 +3,6 @@
 for algorithm in "CRL" "GCBC"; do
   for environment in "antmaze-medium-navigate-v0" "antmaze-medium-explore-v0" "antmaze-medium-stitch-v0"; do
     ./setup_run.sh "$algorithm" "$environment" "discount actor_p_trajgoal" "./logs/2025-02-10_${algorithm}_${environment}_32configurations_discount-actorptrajgoal-5seeds"
-    python -m gcrl_landscapes.main submit --logdir "./logs/2025-02-10_${algorithm}_${environment}_32configurations_discount-actorptrajgoal-5seeds" --phase 0 --n_seeds 5 --tasks_per_node 5 --jobname "${algorithm}_${environment}_5seeds"
+    python -m gcrl_landscapes.main submit --logdir "./logs/2025-02-10_${algorithm}_${environment}_32configurations_discount-actorptrajgoal-5seeds" --phase 0 --n_seeds 5 --tasks_per_node 5 --jobname "${algorithm}_${environment}_5seeds" --partition "ai,tnt,gpu"
   done
 done
