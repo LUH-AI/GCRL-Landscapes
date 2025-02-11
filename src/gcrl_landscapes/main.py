9122f9195e0fcefa6200c93df5dcd22e800fe54a
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-11 22:34:26 +0100

    fix mistake

--- src/gcrl_landscapes/main.py
+++ src/gcrl_landscapes/main.py
@@ -218,7 +218,7 @@ def submit(args: argparse.Namespace) -> None:
     executor.update_parameters(
         cpus_per_task=4,
         slurm_time=int(60 * args.tasks_per_node * ((200000 - args.phase) / 200000)),  # this overestimates, keep safety margin
-        slurm_gpus_per_node=8,
+        slurm_gpus_per_node=1,
         tasks_per_node=args.tasks_per_node,
         slurm_mem_per_cpu="1G",
         slurm_array_parallelism=50,
