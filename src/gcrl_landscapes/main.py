1a856332b662b7bbf97874fe6f3f50e3a0aa8ab8
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-11 07:59:15 +0100

    update time estimation based on phase

--- src/gcrl_landscapes/main.py
+++ src/gcrl_landscapes/main.py
@@ -217,7 +217,7 @@ def submit(args: argparse.Namespace) -> None:
     executor = submitit.AutoExecutor(folder=str(args.logdir / "submitit" / "%j"))
     executor.update_parameters(
         cpus_per_task=4,
-        slurm_time=60 * args.tasks_per_node,  # this overestimates, keep safety margin
+        slurm_time=60 * args.tasks_per_node * ((200000 - args.phase) / 200000),  # this overestimates, keep safety margin
         slurm_gpus_per_node=1,
         tasks_per_node=args.tasks_per_node,
         slurm_mem_per_cpu="1G",
