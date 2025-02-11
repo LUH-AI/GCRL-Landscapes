2d63985097376873fc973657a7df6c41a516b7da
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-11 19:44:13 +0100

    shorten commands as to hopefully not trigger "oserror 7 argument list too long"

--- train_all.sh
+++ train_all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 for algorithm in "CRL" "GCBC"; do
-  for environment in "antmaze-medium-navigate-v0" "antmaze-medium-explore-v0" "antmaze-medium-stitch-v0"; do
-    ./setup_run.sh "$algorithm" "$environment" "discount actor_p_trajgoal" "./logs/2025-02-11_${algorithm}_${environment}_32configurations_discount-actorptrajgoal-5seeds"
-    python -m gcrl_landscapes.main submit --logdir "./logs/2025-02-11_${algorithm}_${environment}_32configurations_discount-actorptrajgoal-5seeds" --phase 0 --n_seeds 5 --tasks_per_node 5 --jobname "${algorithm}_${environment}_5seeds" --partition "ai,tnt,gpu"
+  for environment in "navigate" "explore" "stitch"; do
+    ./setup_run.sh "$algorithm" "antmaze-medium-${environment}-v0" "discount actor_p_trajgoal" "./logs/${algorithm}_${environment}_32c_disc-acttraj"
+    python -m gcrl_landscapes.main submit --logdir "./logs/${algorithm}_${environment}_32c_disc-acttraj" --phase 0 --n_seeds 5 --tasks_per_node 5 --jobname "${algorithm}_${environment}" --partition "ai,tnt,gpu"
   done
 done
