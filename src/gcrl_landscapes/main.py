18a8e8c43c128bd8df425926b287387992b79ad6
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-12 11:48:05 +0100

    Revert the changes done for debugging, as these are not the culprit
    Revert "remove gitpython as it may be the culprit"
    
    This reverts commit 5990bece89b0212025e16779e425dfac7d50e91a.
    
    Revert "environ debug prints in train"
    
    This reverts commit f8dfe5128e90412c85d24544405f0a0a4ab8e039.

--- src/gcrl_landscapes/main.py
+++ src/gcrl_landscapes/main.py
@@ -4,6 +4,7 @@ import argparse
 from datetime import datetime
 from pathlib import Path
 import json
+import git
 import toml
 import logging
 from ml_collections import FrozenConfigDict
@@ -41,6 +42,10 @@ def run_setup(args: argparse.Namespace) -> None:
     # Log metadata to file
     metadata = {
         "arguments": args.__dict__,
+        "git": {
+            "commit": git.Repo(".", search_parent_directories=True).head.object.hexsha,
+            "branch": git.Repo(".", search_parent_directories=True).active_branch.name,
+        },
         "time": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
     }
     with open(args.logdir / "info.toml", "w") as f:
@@ -144,6 +149,10 @@ def run_config(
             "agent_path": agent_path,
             "configuration_index": configuration_index,
             "seed": seed,
+        },
+        "git": {
+            "commit": git.Repo(".", search_parent_directories=True).head.object.hexsha,
+            "branch": git.Repo(".", search_parent_directories=True).active_branch.name,
         },
         "time": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
     }
