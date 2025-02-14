820d5a879566551f2ac470180123a66fe5b7ec84
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-14 17:58:18 +0100

    plotting nearer to understand new data

--- src/gcrl_landscapes/plot.py
+++ src/gcrl_landscapes/plot.py
@@ -1,44 +1,35 @@
 import argparse
-from util.data import ResultsPerStep, PhaseResult, phase_results_to_pandas
-import json
+from .util.data import (
+    ResultsPerStep,
+    PhaseResult,
+    phase_results_to_pandas,
+    read_results_from_zip,
+)
 from pathlib import Path
-from plots.triple_gp import TripleGPModel, create_contour_plot
+from .plots.triple_gp import TripleGPModel, create_contour_plot
 import numpy as np
-from configurations import get_config_space
+from .configurations import get_config_space
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
+from typing import Any
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--logfolder", type=Path, required=True)
-    parser.add_argument("--hp_list", nargs="+", type=str)
-    args = parser.parse_args()
 
-    with open(args.logfolder / "results.json", "r") as f:
-        results_raw = json.load(f)
-
-    results: ResultsPerStep[PhaseResult] = ResultsPerStep(
-        {
-            int(step): PhaseResult.from_dict(result)
-            for step, result in results_raw.items()
-        }
-    )
-
-    results_pandas = phase_results_to_pandas(results)
+def plot(results_pandas: pd.DataFrame, folder: Path, info: dict[str, Any]):
     hp_full_list = results_pandas.columns[results_pandas.columns.str.startswith("hp.")]
-    # keep only hyperparameters that are actually changed/do not stay constant
-    assert all(hp in hp_full_list for hp in args.hp_list)
+    hp_list = [f"hp.{hp_name}" for hp_name in info["arguments"]["hyperparameters"]]
+    assert all(hp in hp_full_list for hp in hp_list)
 
+    phase_starts = sorted(results_pandas["phase_start"].unique().tolist())
     phase_results = [
         (
             phase,
             results_pandas[
                 (results_pandas["eval_step"] == phase)
-                & (results_pandas["phase"] == phase)
+                & (results_pandas["phase_start"] == phase_start)
             ],
         )
-        for phase in results_pandas["phase"].unique()
+        for phase_start, phase in zip(phase_starts, info["arguments"]["phases"])
     ]
 
     for phase, phase_result in phase_results:
@@ -50,7 +41,7 @@ if __name__ == "__main__":
             phase_result_copy,
             np.float64,
             y_col="success",
-            hp_names=args.hp_list,
+            hp_names=hp_list,
             configspace=get_config_space(""),
         )
         model.fit()
@@ -60,7 +51,7 @@ if __name__ == "__main__":
             y_dim=1,
             z_dim="Eval Returns",
             bounds=[0, 1],
-            filename=args.logfolder / f"igpr_{phase}.png",
+            filename=folder / f"igpr_{phase}.png",
         )
 
         # Create plot without using gaussian processes
@@ -84,4 +75,27 @@ if __name__ == "__main__":
 
         c = plt.pcolormesh(x0i, x1i, yi, cmap="rocket", vmin=yi.min(), vmax=yi.max())
         plt.colorbar(c, label="Success")
-        plt.savefig(args.logfolder / f"nearest_{phase}.png")
+        plt.savefig(folder / f"nearest_{phase}.png")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--zipfile", type=Path, required=True)
+    args = parser.parse_args()
+
+    results: dict[str, tuple[dict, ResultsPerStep[PhaseResult]]] = (
+        read_results_from_zip(args.zipfile)
+    )
+
+    results_pandas = {
+        identifier: (run_info, phase_results_to_pandas(phase_results))
+        for identifier, (run_info, phase_results) in results.items()
+    }
+
+    for prefix, (run_info, results_df) in results_pandas.items():
+        folder = (
+            Path("plots")
+            / f"{run_info['arguments']['agent']}_{'-'.join(run_info['arguments']['hyperparameters'])}"
+        )
+        folder.mkdir(exist_ok=True, parents=True)
+        plot(results_df, folder, run_info)
