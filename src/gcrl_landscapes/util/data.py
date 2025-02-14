29aa80b7a8d7d8cca3a88325720c01a93b1fb593
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-14 17:12:23 +0100

    parsing of zip file finished

--- src/gcrl_landscapes/util/data.py
+++ src/gcrl_landscapes/util/data.py
@@ -63,21 +63,24 @@ class EvalTrajectory(tuple[ResultsPerStep[EvaluationResult], ResultsPerStep[Path
         )
 
 
-class PhaseResult(dict[FrozenConfigDict, EvalTrajectory]):
+class PhaseResult(dict[FrozenConfigDict, list[EvalTrajectory]]):
     def to_dict(self) -> dict:
         return {
-            config.to_json(): eval_trajectory.to_json()
-            for config, eval_trajectory in self.items()
+            config.to_json(): [
+                eval_trajectory.to_json() for eval_trajectory in eval_trajectories
+            ]
+            for config, eval_trajectories in self.items()
         }
 
     @classmethod
     def from_dict(cls, raw_result: dict) -> "PhaseResult":
         return PhaseResult(
             {
-                FrozenConfigDict(json.loads(config)): EvalTrajectory.from_json(
-                    eval_trajectory
-                )
-                for config, eval_trajectory in raw_result.items()
+                FrozenConfigDict(json.loads(config)): [
+                    EvalTrajectory.from_json(eval_trajectory)
+                    for eval_trajectory in eval_trajectories
+                ]
+                for config, eval_trajectories in raw_result.items()
             }
         )
 
@@ -165,8 +168,72 @@ def read_results_from_zip(zippath: Path) -> dict[str, ResultsPerStep[PhaseResult
             for info_name in top_level_info_names
         }
 
+    def get_configurations(
+        filenames: list[str], zip_file: zipfile.ZipFile
+    ) -> dict[int, FrozenConfigDict]:
+        configuration_regex = re.compile(r"^.*configurations/configuration_(\d+).json$")
+        regex_matches = [configuration_regex.match(filename) for filename in filenames]
+        configurations = {}
+        for configuration_idx, configuration_path in {
+            int(match.group(1)): match.group(0) for match in regex_matches if match
+        }.items():
+            with zip_file.open(configuration_path) as f:
+                configurations[configuration_idx] = FrozenConfigDict(
+                    json.loads(f.read().decode(encoding="utf-8"))
+                )
+        return configurations
+
+    def extract_results(
+        filenames: list[str],
+        configurations: dict[int, FrozenConfigDict],
+        zip_file: zipfile.ZipFile,
+    ) -> ResultsPerStep[PhaseResult]:
+        def parse_filename(filename: str) -> tuple[int, int, int]:
+            """Return (configuration_number, phase, seed)"""
+            match = re.search(
+                r".*configuration_(\d+)/phase_(\d+)/seed_(\d+).*", filename
+            )
+            return int(match.group(1)), int(match.group(2)), int(match.group(3))
+
+        results: ResultsPerStep[PhaseResult] = ResultsPerStep({})
+        for filename in [
+            filename
+            for filename in filenames
+            if re.search(r".*eval_trajectory\.json$", filename)
+        ]:
+            with zip_file.open(filename) as f:
+                file_content = f.read().decode(encoding="utf-8")
+                config_num, phase, seed = parse_filename(filename)
+                if phase not in results:
+                    results[phase] = PhaseResult({})
+                if configurations[config_num] not in results[phase]:
+                    results[phase][configurations[config_num]] = []
+                results[phase][configurations[config_num]].append(
+                    EvalTrajectory.from_json(json.loads(file_content))
+                )
+
+        return results
+
     with zipfile.ZipFile(zippath, "r") as zip_file:
         filenames: list[str] = zip_file.namelist()
-        prefix_run_mappings = get_prefix_run_mappings(filenames, zip_file)  # noqa: F841
+        prefix_run_mapping = get_prefix_run_mappings(filenames, zip_file)
+        prefix_filenames_mapping = {
+            prefix: [
+                filename for filename in filenames if re.match(f"^{prefix}.*", filename)
+            ]
+            for prefix in prefix_run_mapping.keys()
+        }
+        prefix_configuration_mapping: dict[str, dict[int, FrozenConfigDict]] = {
+            prefix: get_configurations(filenames, zip_file)
+            for prefix, filenames in prefix_filenames_mapping.items()
+        }
+        prefix_phase_results_mapping = {
+            prefix: extract_results(
+                prefix_filenames_mapping[prefix],
+                prefix_configuration_mapping[prefix],
+                zip_file,
+            )
+            for prefix in prefix_run_mapping.keys()
+        }
 
-    return {}
+    return prefix_phase_results_mapping
