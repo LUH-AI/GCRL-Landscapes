e6df74bb87c9249e6ffb6b75844ebe458ea3a119
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-17 09:59:32 +0100

    shorter runs for faster results

--- train_all.sh
+++ train_all.sh
@@ -3,6 +3,6 @@
 for algorithm in "CRL" "GCBC"; do
   for environment in "navigate" "explore" "stitch"; do
     ./setup_run.sh "$algorithm" "antmaze-medium-${environment}-v0" "discount actor_p_trajgoal" "./logs/${algorithm}_${environment}_32c_disc-acttraj"
-    python -m gcrl_landscapes.main submit --logdir "./logs/${algorithm}_${environment}_32c_disc-acttraj" --phase 0 --n_seeds 5 --tasks_per_node 4 --jobname "${algorithm}_${environment}" --partition "ai,tnt"
+    python -m gcrl_landscapes.main submit --logdir "./logs/${algorithm}_${environment}_32c_disc-acttraj" --phase 0 --n_seeds 3 --tasks_per_node 4 --jobname "${algorithm}_${environment}" --partition "ai,tnt"
   done
 done
