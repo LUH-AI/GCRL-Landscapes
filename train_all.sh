95ec80778335433294c7e5d85f1bab1a501cf103
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-02-17 10:35:48 +0100

    add jax compilation cache dir

--- train_all.sh
+++ train_all.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export JAX_COMPILATION_CACHE_DIR="${BIGWORK}/jax_cache"
+
 for algorithm in "CRL" "GCBC"; do
   for environment in "navigate" "explore" "stitch"; do
     ./setup_run.sh "$algorithm" "antmaze-medium-${environment}-v0" "discount actor_p_trajgoal" "./logs/${algorithm}_${environment}_32c_disc-acttraj"
