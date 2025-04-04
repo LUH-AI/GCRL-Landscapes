684b9e384d800e2dbb90551a6cd0584f8c5c5a26
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-04-04 12:34:27 +0200

    save goal distance

--- src/gcrl_landscapes/evaluate.py
+++ src/gcrl_landscapes/evaluate.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 from ogbench.impls.utils.evaluation import evaluate
 import numpy as np
 from ml_collections import ConfigDict
+import warnings
 
 
 def evaluate_wrapper(
@@ -30,11 +31,36 @@ def evaluate_wrapper(
     eval_metrics = {
         metric: np.mean([info[metric] for info in eval_info]) for metric in metrics
     }
+
     length_of_trajectories = [
         [len(traj["reward"]) for traj in trajs_task] for trajs_task in trajs
     ]
-    for eval_info_task, length_of_trajectories_task in zip(
-        eval_info, length_of_trajectories
+    warnings.warn("evaluation is currently only supported for two-dimensional goals")
+    goal_start_distances = [
+        # [:2] -> two-dimensional
+        [
+            float(np.linalg.norm(traj["info"][0]["goal"][:2] - traj["info"][0]["xy"]))
+            for traj in trajs_task
+        ]
+        for trajs_task in trajs
+    ]
+    goal_end_distances = [
+        # [:2] -> two-dimensional
+        [
+            float(np.linalg.norm(traj["info"][-1]["goal"][:2] - traj["info"][-1]["xy"]))
+            for traj in trajs_task
+        ]
+        for trajs_task in trajs
+    ]
+    for (
+        eval_info_task,
+        length_of_trajectories_task,
+        goal_start_distances_task,
+        goal_end_distances_task,
+    ) in zip(
+        eval_info, length_of_trajectories, goal_start_distances, goal_end_distances
     ):
         eval_info_task["length_of_trajectories"] = length_of_trajectories_task
+        eval_info_task["goal_start_distances"] = goal_start_distances_task
+        eval_info_task["goal_end_distances"] = goal_end_distances_task
     return eval_info, eval_metrics, trajs, renders
