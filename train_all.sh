05274d3adace540fa63222f897667e05c2c30e21
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-04-06 10:08:56 +0200

    calculate running time more dynamically

--- train_all.sh
+++ train_all.sh
@@ -2,9 +2,9 @@
 
 export JAX_COMPILATION_CACHE_DIR="${BIGWORK}/jax_cache"
 
-for algorithm in "CRL" "GCBC"; do
-  for environment in "navigate" "explore" "stitch"; do
-    ./setup_run.sh "$algorithm" "antmaze-medium-${environment}-v0" "discount actor_p_trajgoal" "./logs/${algorithm}_${environment}_32c_disc-acttraj"
-    python -m gcrl_landscapes.main submit --logdir "./logs/${algorithm}_${environment}_32c_disc-acttraj" --n_seeds 5 --tasks_per_node 5 --jobname "${algorithm}_${environment}" --partition "ai,tnt"
-  done
+for environment in "navigate" "explore" "stitch"; do
+  ./setup_run.sh "$algorithm" "antmaze-medium-${environment}-v0" "discount actor_p_trajgoal" "./logs/CRL_${environment}_32c_disc-acttraj"
+  ./setup_run.sh "$algorithm" "antmaze-medium-${environment}-v0" "discount actor_p_trajgoal" "./logs/GCBC_${environment}_32c_disc-acttraj"
+  python -m gcrl_landscapes.main submit --logdir "./logs/CRL_${environment}_32c_disc-acttraj" --n_seeds 5 --tasks_per_node 5 --jobname "${algorithm}_${environment}" --partition "ai,tnt" --min_per_mill_steps 150
+  python -m gcrl_landscapes.main submit --logdir "./logs/GCBC_${environment}_32c_disc-acttraj" --n_seeds 5 --tasks_per_node 5 --jobname "${algorithm}_${environment}" --partition "ai,tnt" --min_per_mill_steps 300
 done
