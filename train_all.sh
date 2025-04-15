862e30d4a75729bf208c4a74f35eda399ef2e7f5
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-04-15 09:48:22 +0200

    discount hp as default

--- train_all.sh
+++ train_all.sh
@@ -3,7 +3,7 @@
 export JAX_COMPILATION_CACHE_DIR="${BIGWORK}/jax_cache"
 
 for environment in "navigate" "stitch"; do
-  ./setup_run.sh "CRL" "antmaze-medium-${environment}-v0" "lr actor_p_trajgoal" "./logs/CRL_${environment}_32c_disc-acttraj"
+  ./setup_run.sh "CRL" "antmaze-medium-${environment}-v0" "discount actor_p_trajgoal" "./logs/CRL_${environment}_32c_disc-acttraj"
   ./setup_run.sh "GCBC" "antmaze-medium-${environment}-v0" "discount lr" "./logs/GCBC_${environment}_32c_disc-acttraj"
    python -m gcrl_landscapes.main submit --logdir "./logs/CRL_${environment}_32c_disc-acttraj" --n_seeds 3 --tasks_per_node 5 --jobname "CRL_${environment}" --partition "ai,tnt" --min_per_mill_steps 300
   python -m gcrl_landscapes.main submit --logdir "./logs/GCBC_${environment}_32c_disc-acttraj" --n_seeds 3 --tasks_per_node 5 --jobname "GCBC_${environment}" --partition "ai,tnt" --min_per_mill_steps 200
