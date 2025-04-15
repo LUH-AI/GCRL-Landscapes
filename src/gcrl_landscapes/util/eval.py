415556706fe73f452d02bf574d9c4541f1ce0af1
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-04-15 17:58:10 +0200

    plot dispersion

--- src/gcrl_landscapes/util/eval.py
+++ src/gcrl_landscapes/util/eval.py
@@ -4,3 +4,10 @@ import numpy as np
 def cvar(values: np.ndarray, confidence_level: int = 5) -> np.floating:
     tail = values[values < np.percentile(values, confidence_level)]
     return np.mean(tail)
+
+
+def iqr(values: np.ndarray, confidence_levels: tuple[int, int]) -> np.floating:
+    return np.abs(
+        np.percentile(values, confidence_levels[0])
+        - np.percentile(values, confidence_levels[1])
+    )
