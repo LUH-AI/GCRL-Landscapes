f7a4a6138e89f49454f767e533eb1ba94e478d3b
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-04-25 18:32:03 +0200

    train script bug fixes

--- train_all.sh
+++ train_all.sh
@@ -39,20 +39,10 @@ declare -a -r environments=(
   "cube-single-play-v0"
   "cube-double-play-v0"
   "powderworld-easy-play-v0"
-  "visual-antmaze-medium-navigate-v0"
-  "visual-antmaze-medium-stitch-v0"
-  "visual-antmaze-large-navigate-v0"
-  "visual-antmaze-large-stitch-v0"
-  "visual-humanoidmaze-medium-navigate-v0"
-  "visual-humanoidmaze-medium-stitch-v0"
-  "visual-humanoidmaze-large-navigate-v0"
-  "visual-humanoidmaze-large-stitch-v0"
-  "visual-cube-single-play-v0"
-  "visual-cube-double-play-v0"
 )
 
 # cache autotuning results to not having to recompile them on every run
-if [ -z ${BIGWORK+x}]; then
+if [ -z ${BIGWORK+x} ]; then
   export JAX_COMPILATION_CACHE_DIR="/tmp/jax_cache"
 else
   export JAX_COMPILATION_CACHE_DIR="${BIGWORK}/jax_cache"
@@ -63,6 +53,6 @@ for environment in "${environments[@]}"; do
     full_log_dir="${logdir}/${agent}_${environment}_${numconfigurations}c_${hyperparameters// /-}"
 
     python -m gcrl_landscapes.main setup --agent "$agent" --dataset "$environment" --n_configurations "$numconfigurations" --phases $phases --eval_steps $evalsteps --eval_episodes "$numevalepisodes" --hyperparameters $hyperparameters --logdir "$full_log_dir"
-    python -m gcrl_landscapes.main submit --logdir "$full_log_dir" --n_seeds "$n_seeds" --tasks_per_node "$taskspernode" --jobname "${agent}-${environment}" --partition "$partitions" --min_per_mill_steps "${agent_min_per_mill_steps[${agent}]}"
+    python -m gcrl_landscapes.main submit --logdir "$full_log_dir" --n_seeds "$nseeds" --tasks_per_node "$taskspernode" --jobname "${agent}-${environment}" --partition "$partitions" --min_per_mill_steps "${agent_min_per_mill_steps[${agent}]}"
   done
 done
