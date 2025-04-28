ff26c3aaeaa3ee1d0c5a833ece6c0fa056b90594
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-04-28 15:36:27 +0200

    dont send emails for ended jobs

--- src/gcrl_landscapes/main.py
+++ src/gcrl_landscapes/main.py
@@ -296,7 +296,7 @@ def submit(args: argparse.Namespace) -> None:
             slurm_partition=args.partition,
             slurm_job_name=args.jobname,
             slurm_mail_user="m.toepperwien@stud.uni-hannover.de",
-            slurm_mail_type="FAIL,END",
+            slurm_mail_type="FAIL",
             slurm_additional_parameters={"dependency": f"afterok:{array_id}"}
             if array_id
             else {},
