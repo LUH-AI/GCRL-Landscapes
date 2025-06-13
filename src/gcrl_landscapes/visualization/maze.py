6724f8ba9982c6b51b1566567d4c499b5cffe074
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-06-13 16:52:39 +0200

    visualize explore datasets

--- src/gcrl_landscapes/visualization/maze.py
+++ src/gcrl_landscapes/visualization/maze.py
@@ -1,17 +1,24 @@
-import ogbench
-
-import matplotlib
+from ogbench import make_env_and_datasets
+import re
 import numpy as np
-from matplotlib import figure
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from PIL import Image, ImageOps
+from matplotlib import colormaps
+from matplotlib.colors import to_rgb as matplotlib_color_to_rgb
+from PIL import Image, ImageOps, ImageDraw
+import argparse
+from functools import reduce
+from pathlib import Path
 
 FIGSIZE = (10, 10)
+MAX_IMAGE_SIZE = 1000
+SUPERSAMPLING_FACTOR = 10
 
 
 def to_trajectories(
     dataset: dict[str, np.ndarray], batch_size: int | None = None, replace: bool = False
 ) -> list[dict[str, np.ndarray]]:
+    if batch_size is not None and batch_size < 1:
+        return []
+
     assert all(
         [dataset_member in dataset for dataset_member in ["observations", "terminals"]]
     )
@@ -51,81 +58,118 @@ def get_2d_colors(points, min_point, max_point):
     return colors
 
 
-def visualize_trajs(trajs: list[dict[str, np.ndarray]]) -> Image.Image:
-    """Visualize x-y trajectories in locomotion environments.
-
-    Adapted from OGBench (Park et al. 2025).
-    """
-
-    def xy_to_ij(
-        xy: np.ndarray,
-        x_offset: float = 4.0,
-        y_offset: float = 4.0,
-        maze_unit: float = 4.0,
-    ) -> np.ndarray:
-        # See ogbench MazeEnv
-        i = (xy[:, 0] + x_offset) / maze_unit
-        j = (xy[:, 1] + y_offset) / maze_unit
-        return np.column_stack((i, j))
-
-    matplotlib.use("Agg")
-
-    fig = figure.Figure(tight_layout=True, figsize=FIGSIZE)
-    canvas = FigureCanvas(fig)
-    ax = fig.add_subplot()
-
-    max_xy = 0.0
-    for traj in trajs:
-        xy = xy_to_ij(traj["observations"][:, :2])
-        # direction = np.array([info["direction"] for info in traj["info"]])
-        # color = get_2d_colors(direction, [-1, -1], [1, 1])
-        for i in range(len(xy) - 1):
-            ax.plot(xy[i : i + 2, 0], xy[i : i + 2, 1], linewidth=0.7)
-        max_xy = max(max_xy, np.abs(xy).max() * 1.2)
-
-    plot_axis = [-max_xy, max_xy, -max_xy, max_xy]
-    plot_axis = [0, 7, 0, 7]
-    ax.axis(plot_axis)
-    ax.set_aspect("equal")
-
-    fig.tight_layout()
-    canvas.draw()
-    rgba = np.asarray(canvas.buffer_rgba())
-
-    rgba[:, :, 3] = (255 * (rgba[:, :, :3] != 255).any(axis=2)).astype(np.uint8)
-
-    foreground = ImageOps.flip(Image.fromarray(rgba).resize((1000, 1000)))
-    foreground.save("test.png")
-
-    return foreground
+def xy_to_ij(
+    xy: np.ndarray,
+    env,  # no type hint, as ogbench creates dynamic types during runtime
+) -> np.ndarray:
+    # See ogbench MazeEnv
+    i = (xy[:, 0] + env._offset_x) / env._maze_unit
+    j = (xy[:, 1] + env._offset_y) / env._maze_unit
+    return np.column_stack((i, j))
 
 
-env, train_dataset, val_dataset = ogbench.make_env_and_datasets(
-    "antmaze-medium-navigate-v0",
-    height=FIGSIZE[0] * 100,
-    width=FIGSIZE[1] * 100,
-)  # type: ignore
+def visualize_trajs(
+    trajs: list[dict[str, np.ndarray]], background: Image.Image, env
+) -> Image.Image:
+    """Visualize x-y trajectories in maze environments."""
+    visualized_image = background.copy()
+    draw = ImageDraw.Draw(visualized_image)
 
-# move goal and ant out of picture
-env.unwrapped.set_goal(goal_xy=(-10, -10))
-env.unwrapped.set_xy((-10, -10))
+    colors = [colormaps.get_cmap("tab20")(i) for i in range(len(trajs))]
+    for traj, color in zip(trajs, colors):
+        ij = xy_to_ij(traj["observations"][:, :2], env)
+        draw.line(
+            ((ij + 0.5) / env.maze_map.transpose().shape * background.size).tolist(),
+            width=2 * SUPERSAMPLING_FACTOR,
+            fill=tuple(
+                [round(channel * 255) for channel in matplotlib_color_to_rgb(color)]
+            ),
+        )
+
+    return visualized_image
 
 
-all_trajectories = to_trajectories(train_dataset, batch_size=3)
-all_xy = np.concatenate(
-    [traj["observations"][:, :2] for traj in all_trajectories]
-)  # .transpose(2, 0, 1).reshape(2, -1).transpose()
-
-
-visualized_env = env.unwrapped.render()
-visualized_trajs = visualize_trajs(all_trajectories)
-
-background = ImageOps.invert(
-    Image.fromarray(env.unwrapped.maze_map.astype(np.uint8) * 255).resize(
-        (1000, 1000), resample=Image.Resampling.BOX
+def sample_trajectories(
+    num_dataset_pairs: list[tuple[int, dict[str, np.ndarray]]],
+) -> list[dict[str, np.ndarray]]:
+    return reduce(
+        lambda a, b: a + b,
+        [
+            to_trajectories(dataset, batch_size=num)
+            for num, dataset in num_dataset_pairs
+        ],
     )
-)
 
 
-background.paste(visualized_trajs, (0, 0), visualized_trajs)
-background.save("traj_visualization.png")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", required=True, help="Which dataset to visualize")
+    parser.add_argument(
+        "--num_trajectories",
+        required=False,
+        default=10,
+        type=int,
+        help="How many trajectories to visualize",
+    )
+    args = parser.parse_args()
+    print(args.dataset)
+
+    output_folder = (
+        Path("plots")
+        / "visualizations"
+        / "datasets"
+        / f"{args.num_trajectories}-trajectories"
+    )
+    output_folder.mkdir(exist_ok=True, parents=True)
+
+    explore_mix_match = re.fullmatch(
+        r"^.*explore(?P<explore_share>\d+)(?P<secondtype>[^-]*).*$", args.dataset
+    )
+    if explore_mix_match:
+        base_dataset = re.sub(r"explore\d+", "", args.dataset)
+        explore_dataset = re.sub(r"explore\d+[^-]*", "explore", args.dataset)
+        explore_share = int(explore_mix_match.groupdict()["explore_share"])
+        env, train_base_dataset_raw, val_dataset1_raw = make_env_and_datasets(
+            base_dataset
+        )  # type: ignore
+        _, train_explore_dataset, val_dataset2_raw = make_env_and_datasets(
+            explore_dataset
+        )  # type: ignore
+        num_explore_samples = round(args.num_trajectories * (explore_share / 100))
+        trajectories = sample_trajectories(
+            [
+                (args.num_trajectories - num_explore_samples, train_base_dataset_raw),
+                (num_explore_samples, train_explore_dataset),
+            ]
+        )
+    else:
+        env, train_dataset_raw, val_dataset_raw = make_env_and_datasets(  # type: ignore
+            args.dataset
+        )
+        trajectories = sample_trajectories([(args.num_trajectories, train_dataset_raw)])
+
+    assert len(trajectories) == args.num_trajectories
+    # move goal and ant out of picture
+    env.unwrapped.set_goal(goal_xy=(-10, -10))  # type: ignore
+    env.unwrapped.set_xy((-10, -10))  # type: ignore
+    visualized_env = env.unwrapped.render()  # type: ignore
+
+    background = ImageOps.contain(
+        ImageOps.invert(
+            Image.fromarray(env.unwrapped.maze_map.astype(np.uint8) * 255)  # type: ignore
+        ),
+        (MAX_IMAGE_SIZE * SUPERSAMPLING_FACTOR, MAX_IMAGE_SIZE * SUPERSAMPLING_FACTOR),
+        method=Image.Resampling.BOX,
+    )
+
+    visualized_trajs = visualize_trajs(
+        trajectories,
+        ImageOps.colorize(background, black="#303e57", white="white"),
+        env.unwrapped,
+    )
+
+    ImageOps.contain(
+        visualized_trajs,
+        (MAX_IMAGE_SIZE, MAX_IMAGE_SIZE),
+        method=Image.Resampling.LANCZOS,
+    ).save(output_folder / f"trajvisualization_{args.dataset}.png")
