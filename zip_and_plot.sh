8c629ada80972f2c4d66aadf65feb1099012e639
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-06-21 12:45:54 +0200

    longer time limit for ZipAndPlot

--- zip_and_plot.sh
+++ zip_and_plot.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --partition=ai,tnt
 #SBATCH --job-name=ZipAndPlot
-#SBATCH --time=02:00:00
+#SBATCH --time=10:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4G
