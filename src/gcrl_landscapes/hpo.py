f62d5042c8b481ec2432d69536f7c519deb5e919
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-06-21 16:20:53 +0200

    untested hpo_target implementation

--- src/gcrl_landscapes/hpo.py
+++ src/gcrl_landscapes/hpo.py
@@ -1,11 +1,28 @@
+from pathlib import Path
 import hydra
 from omegaconf import DictConfig
+from .configurations import hydra_to_ogbench_config
+from .submission import train_wrapper
 
 
 @hydra.main(config_path="../../configs", config_name="hpo_crl", version_base="1.1")
-def hpo_target(cfg: DictConfig) -> float:
-    print(cfg)
-    return 10
+def hpo_target(hydra_config: DictConfig) -> float:
+    config = hydra_to_ogbench_config(hydra_config)
+    eval_trajectory = train_wrapper(
+        agent_name=config["agent_name"],  # type: ignore
+        agent_path=None,
+        dataset_name=config["env"],  # type: ignore
+        already_trained_steps=0,
+        eval_steps=config["training_steps"],  # type: ignore
+        save_steps=[],
+        eval_episodes=50,
+        configuration=config,
+        run_log_dir=Path("./"),
+        tasks_per_node_parallel=4,
+        seed=config["seed"],  # type: ignore
+    )
+    eval_results = eval_trajectory[0]
+    return eval_results[max(eval_results.keys())].success
 
 
 if __name__ == "__main__":
