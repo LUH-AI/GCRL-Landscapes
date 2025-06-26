76201526a7df91f2648725107125033ad85bf14c
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-06-26 18:22:27 +0200

    format bash scripts

--- convergence.sh
+++ convergence.sh
@@ -72,12 +72,37 @@ for environment in "${environments[@]}"; do
     echo "Starting job for ${agent}"
     full_log_dir="${logdir}/${agent}_${environment}_${numconfigurations}c_${hyperparameters// /-}"
 
-    python -m gcrl_landscapes.main setup --agent "$agent" --dataset "$environment" --n_configurations "$numconfigurations" --convergence_zip "$convergencezip" --phase_percentages $phasepercentages --final_performance_percentage $finalperformancepercentage --eval_episodes "$numevalepisodes" --hyperparameters $hyperparameters --logdir "$full_log_dir" --final_step_is_phase --extra_eval_steps $extraevalsteps --actor_loss "$actorloss"
-    python -m gcrl_landscapes.main submit --logdir "$full_log_dir" --n_seeds "$nseeds" --tasks_per_node_total "$taskspernodetotal" --tasks_per_node_parallel "$taskspernodeparallel" --mem_per_cpu "$mempercpu" --jobname "${agent}-${environment}-${actorloss}" --partition "$partitions" --min_per_mill_steps "${agent_min_per_mill_steps[${agent}]}" --basetime 120
+    python -m gcrl_landscapes.main setup \
+      --agent "$agent" \
+      --dataset "$environment" \
+      --n_configurations "$numconfigurations" \
+      --convergence_zip "$convergencezip" \
+      --phase_percentages $phasepercentages \
+      --final_performance_percentage $finalperformancepercentage \
+      --eval_episodes "$numevalepisodes" \
+      --hyperparameters $hyperparameters \
+      --logdir "$full_log_dir" \
+      --final_step_is_phase \
+      --extra_eval_steps $extraevalsteps \
+      --actor_loss "$actorloss"
+    python -m gcrl_landscapes.main submit \
+      --logdir "$full_log_dir" \
+      --n_seeds "$nseeds" \
+      --tasks_per_node_total "$taskspernodetotal" \
+      --tasks_per_node_parallel "$taskspernodeparallel" \
+      --mem_per_cpu "$mempercpu" \
+      --jobname "${agent \
+      -${environment \
+      -${actorloss}" \
+      --partition "$partitions" \
+      --min_per_mill_steps "${agent_min_per_mill_steps[${agent}]}" \
+      --basetime 120
   done
 done
 
 # Automatically zip and plot after all runs done
 # Currently this waits for all jobs belonging to user and not only the ones submitted here
 dependencies=$(squeue --me -l -r | tail -n +3 | tr -s ' ' | cut -d ' ' -f2 | cut -d '_' -f1 | sort | uniq | paste -s -d':')
-sbatch --output "${logdir}/zip_and_plot_log.txt" -d "afterok:${dependencies}" ./zip_and_plot.sh "$logdir" $(which python)
+sbatch \
+  --output "${logdir}/zip_and_plot_log.txt" \
+  -d "afterok:${dependencies}" ./zip_and_plot.sh "$logdir" $(which python)
