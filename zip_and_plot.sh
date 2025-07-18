a17e7d223f19c501e33b9078c93e236a57ccfda1
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-07-18 08:52:07 +0200

    zipandplot job changes

--- zip_and_plot.sh
+++ zip_and_plot.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --partition=ai,tnt
 #SBATCH --job-name=ZipAndPlot
-#SBATCH --time=04:00:00
+#SBATCH --time=05:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=3G
 #SBATCH --mail-user=m.toepperwien@stud.uni-hannover.de
 #SBATCH --mail-type=END,FAIL
 #SBATCH --get-user-env
@@ -16,5 +16,5 @@ ZIPNAME="${DATE}-${1/\.\//}.zip"
 
 module load GCC/12.2.0 OpenMPI/4.1.4 Armadillo
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${BIGWORK}/usr/lib"
-$2 -m gcrl_landscapes.evaluation.plot --plot_eval_curves --plot_return_distributions --zipfile "$ZIPNAME"
+$2 -m gcrl_landscapes.evaluation.plot --plot_eval_curves --plot_return_distributions --plot_gp_fits --zipfile "$ZIPNAME"
 $2 -m gcrl_landscapes.evaluation.tabular --zipfile "$ZIPNAME"
