910e930dfb0fb5a8424473a15c09d5729c8ced5e
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-07-19 11:41:54 +0200

    fix: setup using "datasets" now

--- src/gcrl_landscapes/evaluation/plot.py
+++ src/gcrl_landscapes/evaluation/plot.py
@@ -464,7 +464,7 @@ if __name__ == "__main__":
             "arguments"
         ]
         agent = setup["agent"]
-        dataset = setup["dataset"]
+        dataset = setup["datasets"]
         if "actor_loss" in setup:
             actor_loss = setup["actor_loss"]
         else:
