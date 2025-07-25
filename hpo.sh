8f39ad46e6e2d15c2d09c686232dc6a1d2b3b01d
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-07-25 17:08:14 +0200

    hpo: no need for submission in submit script and properly parse sbatch

--- hpo.sh
+++ hpo.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#SBATCH --partition=ai,tnt
-#SBATCH --job-name=HPO
-#SBATCH --time=00:10:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4G
-#SBATCH --mail-user=m.toepperwien@stud.uni-hannover.de
-#SBATCH --mail-type=END,FAIL
-#SBATCH --get-user-env
-
 logdir="./logs_hpo"
 declare -a -r datasets=(
   antmaze-medium-explore-v0
@@ -34,9 +24,6 @@ if [ -z "$1" ]
     echo "No agent supplied"
 fi
 
-module load Miniforge3
-conda activate gcrl
-
 for phase in "${phases[@]}"; do
   if [ -z "$job_id" ]
     then
@@ -44,6 +31,11 @@ for phase in "${phases[@]}"; do
     else
       output=$(sbatch --parsable --output "${logdir}/hpo_log_${phase}.txt" --dependency="afterok:${job_id}" hpo_single.sh "$1" "$logdir" "$datasets_str" "$phases_str" "$phase")
   fi
-  env IFS=';' read -r job_id cluster_name <<< "$output"
+  if [[ "$output" == *";"* ]]; then
+    IFS=';' read -r job_id cluster_name <<< "$output"
+  else
+    job_id="$output"
+    cluster_name="unknown"
+  fi
   echo "Submitted job $job_id to cluster $cluster_name for phase $phase"
 done
