1d98b9076e6324e3a59b886d943d0e0697e15540
Jan Malte Töpperwien <m.toepperwien@protonmail.com>
2025-09-21 14:57:02 +0200

    fix(evaluation): fix convergence plots

--- src/gcrl_landscapes/evaluation/plot.py
+++ src/gcrl_landscapes/evaluation/plot.py
@@ -113,35 +113,16 @@ def plot_landscape(
 
 
 def plot_eval_curve(
-    phase: int,
     phase_result: pd.DataFrame,
     y_col: str,
     output_folder: Path,
-    output_per_config_folder: Path,
     y_label: str | None,
 ):
     exploded_phase_result = phase_result[[y_col, "config_index", "eval_step"]].explode(
         y_col
     )  # type: ignore
-    # per configuration
-    for config_index, group in exploded_phase_result.groupby("config_index"):
-        _ = plt.figure()
-        ax = sns.lineplot(
-            data=group,  # type: ignore
-            x="eval_step",
-            y=y_col,
-            errorbar=("ci", 95),
-        )
-        # plt.title(f"{y_label if y_label else y_col}", fontsize=18)
-        ax.set_ylim(0, 1)
-        plt.savefig(
-            output_per_config_folder
-            / f"eval-{y_col}-config_{config_index}-{phase}.png",
-            bbox_inches="tight",
-        )
-        plt.close()
-    ## configuration marginalized
-    _ = plt.figure()
+
+    fig, ax = plt.subplots(figsize=[4, 3])
     ax = sns.lineplot(
         data=exploded_phase_result,  # type: ignore
         x="eval_step",
@@ -149,9 +130,11 @@ def plot_eval_curve(
         errorbar=("ci", 95),
     )
     # plt.title(f"{y_label if y_label else y_col}", fontsize=18)
+    plt.ylabel(y_label if y_label else y_col)
+    plt.xlabel("Training Step")
     ax.set_ylim(0, 1)
     plt.savefig(
-        output_folder / f"eval-{y_col}-{phase}.png",
+        output_folder / f"eval-{y_col}.png",
         bbox_inches="tight",
     )
     plt.close()
